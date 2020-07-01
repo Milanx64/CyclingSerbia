74aa116c2ff374fe4e3cb38f3f0a100773f0f574
Milanx64 <markovic37m@gmail.com>
2020-07-01 14:11:42 +0200

    Some minor changes in code formatting

--- CyclingSerbia/src/main/java/com/springmvc/model/Track.java
+++ CyclingSerbia/src/main/java/com/springmvc/model/Track.java
@@ -100,6 +100,71 @@ public class Track {
 	public void setMountain(Mountain mountain) {
 		this.mountain = mountain;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(cost);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + difficulty;
+		temp = Double.doubleToLongBits(duration);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		temp = Double.doubleToLongBits(length);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((mountain == null) ? 0 : mountain.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Track other = (Track) obj;
+		if (Double.doubleToLongBits(cost) != Double.doubleToLongBits(other.cost))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (difficulty != other.difficulty)
+			return false;
+		if (Double.doubleToLongBits(duration) != Double.doubleToLongBits(other.duration))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (Double.doubleToLongBits(length) != Double.doubleToLongBits(other.length))
+			return false;
+		if (mountain == null) {
+			if (other.mountain != null)
+				return false;
+		} else if (!mountain.equals(other.mountain))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Track [id=" + id + ", name=" + name + ", description=" + description + ", difficulty=" + difficulty
+				+ ", cost=" + cost + ", duration=" + duration + ", length=" + length + ", mountain=" + mountain + "]";
+	}
 	
 	
 }

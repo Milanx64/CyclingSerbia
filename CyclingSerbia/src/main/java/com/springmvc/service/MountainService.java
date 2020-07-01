e4fbb1643fe2c427ce7d73c2661cf8657368c24a
Milanx64 <markovic37m@gmail.com>
2020-07-01 14:20:29 +0200

    Method findByRegion added

--- CyclingSerbia/src/main/java/com/springmvc/service/MountainService.java
+++ CyclingSerbia/src/main/java/com/springmvc/service/MountainService.java
@@ -3,11 +3,14 @@ package com.springmvc.service;
 import java.util.List;
 
 import com.springmvc.model.Mountain;
+import com.springmvc.model.Region;
 
 public interface MountainService {
 	List<Mountain> findAll();
 	
 	Mountain findById(int id);
+	
+	List<Mountain> findByRegion(Region region);
 	
 	void save(Mountain mountain);
 	

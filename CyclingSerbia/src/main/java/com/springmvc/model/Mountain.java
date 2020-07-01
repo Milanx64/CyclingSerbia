eff6fbec753c4292f5505c388ebb3b028bdb5d71
Milanx64 <markovic37m@gmail.com>
2020-07-01 14:10:14 +0200

    Hibernate mapping for region added

--- CyclingSerbia/src/main/java/com/springmvc/model/Mountain.java
+++ CyclingSerbia/src/main/java/com/springmvc/model/Mountain.java
@@ -5,10 +5,13 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 @Entity
@@ -20,8 +23,11 @@ public class Mountain {
 	@Column(name = "name", length = 100, nullable = false, unique = true)
 	private String name;
 	
-	@Column(name = "description", nullable = false, length = 255)
+	@Column(name = "description", nullable = false)
 	private String description;
+	
+	@Column(name="region_id", nullable = false)
+	private int regionId;
 	
 	//Mapping mountain with tracks
 	@OneToMany(mappedBy = "mountain", cascade = CascadeType.ALL)
@@ -30,6 +36,11 @@ public class Mountain {
 	//Mapping mountain with photos
 	@OneToMany(mappedBy = "mountain", cascade = CascadeType.ALL)
 	private List<Photo> photos;
+	
+	//Mapping for the region
+	@OneToOne(cascade = CascadeType.ALL)
+	@PrimaryKeyJoinColumn
+	private Region region;
 	
 	public Integer getId() {
 		return id;
@@ -54,6 +65,15 @@ public class Mountain {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	
+
+	/*public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}*/
 
 	public List<Track> getTracks() {
 		return tracks;
@@ -69,6 +89,70 @@ public class Mountain {
 
 	public void setPhotos(List<Photo> photos) {
 		this.photos = photos;
+	}
+
+	
+	public Region getRegion() {
+		return region;
+	}
+
+	public void setRegion(Region region) {
+		this.region = region;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((photos == null) ? 0 : photos.hashCode());
+		result = prime * result + ((tracks == null) ? 0 : tracks.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Mountain other = (Mountain) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (photos == null) {
+			if (other.photos != null)
+				return false;
+		} else if (!photos.equals(other.photos))
+			return false;
+		if (tracks == null) {
+			if (other.tracks != null)
+				return false;
+		} else if (!tracks.equals(other.tracks))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Mountain [id=" + id + ", name=" + name + ", description=" + description + ", tracks=" + tracks
+				+ ", photos=" + photos + "]";
 	}
 	
 	

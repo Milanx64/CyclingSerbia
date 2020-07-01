345b5264968ebe904055ac96c80d3000e2214293
Milanx64 <markovic37m@gmail.com>
2020-07-01 14:14:04 +0200

    Added methods for showing mountains by region

--- CyclingSerbia/src/main/java/com/springmvc/controller/MountainController.java
+++ CyclingSerbia/src/main/java/com/springmvc/controller/MountainController.java
@@ -2,6 +2,7 @@ package com.springmvc.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +18,11 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.springmvc.model.Mountain;
 import com.springmvc.model.Photo;
+import com.springmvc.model.Region;
 import com.springmvc.model.Track;
 import com.springmvc.service.MountainService;
 import com.springmvc.service.PhotoService;
+import com.springmvc.service.RegionService;
 import com.springmvc.service.TrackService;
 
 @Controller
@@ -36,12 +39,36 @@ public class MountainController {
 	@Autowired
 	PhotoService photoService;
 	
-	@RequestMapping(value = "/show-all-mountains", method = RequestMethod.GET)
-	public String showAllMountains(ModelMap model) {
-		List<Mountain> mountains = mountainService.findAll();
-		model.addAttribute("mountains", mountains);
+	@Autowired
+	RegionService regionService;
+	
+	@RequestMapping(value = "/show-for-", method = RequestMethod.GET)
+	public String showAllMountainsForRegion(@Valid Mountain mountain, BindingResult result, ModelMap model, HttpServletRequest request) {
+		String regionToSearch = request.getParameter("region");
+		if(regionToSearch.equalsIgnoreCase("all")) {
+			List<Mountain> mountains = mountainService.findAll();
+			model.addAttribute("mountains", mountains);
+		}
+		else {
+			Region region = regionService.findByName(regionToSearch);
+			List<Mountain> mountains = mountainService.findByRegion(region);
+			if(mountains.isEmpty()) {
+				model.addAttribute("msg", "No results for " + regionToSearch );
+				model.addAttribute("form", "true");
+			}
+			else {
+				model.addAttribute("msg", "All mountains in " + region.getName());
+			}
+			model.addAttribute("mountains", mountains);
+		}
+		
+		List<Region> regions = regionService.findAll();
+		model.addAttribute("regions", regions);
 		model.addAttribute("loggedinuser", getPrincipal());
-		return "mountain-list";
+		if(getPrincipal() != null) {
+			model.addAttribute("loggedin", true);
+		}
+		return "region-mountain-list";
 	}
 	
 	//Method to add mountain
@@ -50,7 +77,11 @@ public class MountainController {
 		Mountain mountain = new Mountain();
 		model.addAttribute("mountain", mountain);
 		model.addAttribute("edit", false);
+		model.addAttribute("msg", "Add New Mountain");
 		model.addAttribute("loggedinuser", getPrincipal());
+		if(getPrincipal() != null) {
+			model.addAttribute("loggedin", true);
+		}
 		return "mountain-reg";
 			
 	}
@@ -64,8 +95,23 @@ public class MountainController {
 		model.addAttribute("mountain", mountain);
 		model.addAttribute("photos", photos);
 		model.addAttribute("loggedinuser", getPrincipal());
+		if(getPrincipal() != null) {
+			model.addAttribute("loggedin", true);
+		}
 		return "mountain";
 		
+	}
+	
+	//Method to show all mountains
+	@RequestMapping(value = "/show-all-mountains", method= RequestMethod.GET)
+	public String showAllMountains(ModelMap model) {
+		List<Mountain> mountains = mountainService.findAll();
+		model.addAttribute("loggedinuser", getPrincipal());
+		model.addAttribute("mountains", mountains);
+		if(getPrincipal() != null) {
+			model.addAttribute("loggedin", true);
+		}
+		return "mountain-list";
 	}
 	
 	@RequestMapping(value = "/new-mountain", method = RequestMethod.POST)
@@ -78,7 +124,11 @@ public class MountainController {
 		model.addAttribute("mountain", mountain);
 		model.addAttribute("success", "Mountain " + mountain.getName() + " created successfuly");
 		model.addAttribute("loggedinuser", getPrincipal());
-		return "success";
+		if(getPrincipal() != null) {
+			model.addAttribute("loggedin", true);
+		}
+		return showAllMountains(model);
+		//return "mountain-list";
 	}
 	
 	//Metod to edit mountain
@@ -90,7 +140,11 @@ public class MountainController {
 		model.addAttribute("mountain", m);
 		model.addAttribute("track", t);
 		model.addAttribute("edit", true);
+		model.addAttribute("msg", "Edit Mountain Data");
 		model.addAttribute("loggedinuser", getPrincipal());
+		if(getPrincipal() != null) {
+			model.addAttribute("loggedin", true);
+		}
 		return "mountain-reg";
 	}
 	
@@ -100,8 +154,10 @@ public class MountainController {
 			return"mountain-reg";
 		}
 		mountainService.updateMountain(mountain);
-		
-		return "success";
+		if(getPrincipal() != null) {
+			model.addAttribute("loggedin", true);
+		}
+		return "mountain-list";
 		
 	}
 	
@@ -111,6 +167,9 @@ public class MountainController {
 		model.addAttribute("track", track);
 		model.addAttribute("edit", false);
 		model.addAttribute("loggedinuser", getPrincipal());
+		if(getPrincipal() != null) {
+			model.addAttribute("loggedin", true);
+		}
 		return "track-reg";
 		
 	}
@@ -125,7 +184,10 @@ public class MountainController {
 		trackService.saveTrack(track);
 		model.addAttribute("success", "Track succesffuly saved");
 		model.addAttribute("loggedinuser", getPrincipal());
-		return "success";
+		if(getPrincipal() != null) {
+			model.addAttribute("loggedin", true);
+		}
+		return showAllTracks(model);
 		
 	}
 	
@@ -133,8 +195,25 @@ public class MountainController {
 	public String showAllTracks(ModelMap model) {
 		List<Track> tracks = trackService.findAllTracks();
 		model.addAttribute("tracks", tracks);
+		model.addAttribute("loggedinuser", getPrincipal());
+		if(getPrincipal() != null) {
+			model.addAttribute("loggedin", true);
+		}
 		return "track-list";
 		
+	}
+	
+	//id in this method refrds to mountain id
+	@RequestMapping(value = "/show-track-{id}", method = RequestMethod.GET)
+	public String showTrack(@PathVariable int id, ModelMap model) {
+		Mountain mountain = mountainService.findById(id);
+		List<Track> tracks = trackService.findAllTracksOnMountain(mountain);
+		model.addAttribute("tracks", tracks);
+		model.addAttribute("loggedinuser", getPrincipal());
+		if(getPrincipal() != null) {
+			model.addAttribute("loggedin", true);
+		}
+		return "track-list";
 	}
 	
 	@RequestMapping(value = "/edit-track-{id}", method = RequestMethod.GET)
@@ -143,6 +222,9 @@ public class MountainController {
 		model.addAttribute("track", track);
 		model.addAttribute("edit", true);
 		model.addAttribute("loggedinuser".concat(getPrincipal()));
+		if(getPrincipal() != null) {
+			model.addAttribute("loggedin", true);
+		}
 		return "track-reg";
 		
 	}
@@ -156,6 +238,20 @@ public class MountainController {
 		return "success";
 	}
 	
+	@RequestMapping(value="/delete-mountain-{id}", method = RequestMethod.GET)
+	public String deleteMountain(@PathVariable int id) {
+		System.out.println("ID to delete is: " + id);
+		Mountain mountain = mountainService.findById(id);
+		if(mountain == null) {
+			System.out.println("No moutain found");
+		} else {
+			mountainService.delete(mountain);
+			for(Photo p : mountain.getPhotos())
+			photoService.deletePhoto(p);
+		}
+		
+		return "success";
+	}
 	private String getPrincipal() {
 		String username = null;
 		Object principal = SecurityContextHolder.getContext().getAuthentication()

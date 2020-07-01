6ff334e56df11cdc91a7c577838b31e7a473b3ff
Milanx64 <markovic37m@gmail.com>
2020-07-01 13:58:03 +0200

    Method findByRegion(Region region) added

--- CyclingSerbia/src/main/java/com/springmvc/dao/MountainDao.java
+++ CyclingSerbia/src/main/java/com/springmvc/dao/MountainDao.java
@@ -3,11 +3,14 @@ package com.springmvc.dao;
 import java.util.List;
 
 import com.springmvc.model.Mountain;
+import com.springmvc.model.Region;
 
 public interface MountainDao {
 	List<Mountain> findAll();
 	
 	Mountain findById(int id);
+	
+	List<Mountain> findByRegion(Region region);
 	
 	void saveMountain(Mountain mountain);
 	
